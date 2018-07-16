2c195b64e4b86f6db1f2551ecb1f53d33004ae15
RebeccaFransson <rebecca@kollektiva.se>
2018-07-16 13:10:51 +0200

    main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,28 @@
 
 import controllers.SparkController
-import spark.Response
 import spark.Spark
 import utils.SchedulerWorker
 
 
 fun main(args: Array<String>) {
-
     val port = Integer.parseInt("8080")
 
     // Settings
     Spark.port(port)
 
-
-    // Template Server
+    // Start a controller
     SparkController()
 
-
-    // GZIP Everything
-    Spark.after("/*") { _, response: Response -> response.header("Content-Encoding", "gzip") }
-
-    // Init/Start server
+    // Start server
     Spark.init()
 
     println("************************************************")
     println("*****  Spark server started on port $port  ******")
     println("************************************************")
 
-
+    /**
+     * Start a worker
+     */
     SchedulerWorker()
 }
 
