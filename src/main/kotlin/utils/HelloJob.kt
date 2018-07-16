ba2800f9d362d83649bf7076d355be00a3c40f30
RebeccaFransson <rebecca@kollektiva.se>
2018-07-16 13:09:50 +0200

    HelloJob

--- src/main/kotlin/utils/HelloJob.kt
+++ src/main/kotlin/utils/HelloJob.kt
@@ -4,21 +4,28 @@ import org.quartz.JobExecutionException
 import org.quartz.JobExecutionContext
 import org.quartz.Job
 
-
+/**
+ * Purpose of this class is to Print out some text about a job
+ */
 class HelloJob : Job {
 
+    /**
+     * Override execute function.
+     *
+     * This is the job that this class will execute when a job with this class is triggered
+     */
     @Throws(JobExecutionException::class)
     override fun execute(context: JobExecutionContext) {
+        // Name of the job
         val key = context.jobDetail.key
 
+        // Fetch data from job
         val dataMap = context.jobDetail.jobDataMap
-
         val jobSays = dataMap.getString("jobSays")
 
         println("************** Scheduled job *******************")
         println("Hello Job!  Key '$key' is executing.")
         println("Job says: $jobSays")
         println("************************************************")
-
     }
 }
